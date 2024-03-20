ece9ee2303f095e0b0420b8d27962abb944146b4
Muhammad Cikal Merdeka <mcikalmerdeka@gmail.com>
2024-03-20 22:52:12 +0700

    Edit on model deployment file

--- model_deployment.py
+++ model_deployment.py
@@ -1,6 +1,133 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 import streamlit as st
 import joblib
-import numpy as np
+import os
+
+attribute_info = """
+    - Id                 : Unique id of the user
+    - Income             : Income of the user
+    - Age                : Age of the user
+    - Experience         : Professional experience of the user in years
+    - Married/Single     : Marital status (married or single)
+    - House_Ownership    : Homeownership status (owned, rented, or norent_noown)
+    - Car_Ownership      : Carownership status (yes or no)
+    - Profession         : Profession
+    - CITY               : City of residence
+    - STATE              : State of residence
+    - CURRENT_JOB_YRS    : Years of experience in the current job
+    - CURRENT_HOUSE_YRS  : Number of years in the current residence
+    - Risk_Flag          : Defaulted on a loan (0 : not default, 1 : default/failed to pay)
+    """
+
+def load_model(model_file):
+    loaded_model = joblib.load(open(os.path.join(model_file), 'rb'))
+    return loaded_model
+
+def run_ml_app() :
+    st.subheader('Machine Learning Section')
+    with st.expander('Attribute Info') :
+        st.markdown(attribute_info)
+
+    st.subheader('Input Your Data')
+    with st.form('My Data') :
+        # Add input elements for the encoded features using selectbox and number_input
+        income = st.number_input('Income', 10310, 9999938)
+        age = st.number_input('Age', 21, 79)
+        experience = st.number_input('Experience', 0, 20)
+        marital_status = st.selectbox('Marital Status', ['single' 'married'])
+        house_ownership = st.selectbox('House Ownership', ['rented' 'norent_noown' 'owned'])
+        car_ownership = st.selectbox('Car Ownership', ['no' 'yes'])
+        profession = st.selectbox('Profession')
+        city = st.selectbox('City')
+        state = st.selectbox('State')
+        current_job_years = st.number_input('Current Job Years', 0, 14)
+        current_house_years = st.number_input('Current House Years', 10, 14)
+
+        submitted = st.form_submit_button('Submit')
+    
+    if submitted :
+        with st.expander('Variable Dictionary...') :
+            result = {
+                'Income' : income,
+                'Age' : age,
+                'Experience' : experience,
+                'Marital Status' : marital_status,
+                'House Ownership' : house_ownership,
+                'Car Ownership' : car_ownership,
+                'Profession' : profession,
+                'City' : city,
+                'State' : state,
+                'Current Job Years' : current_job_years,
+                'Current House Years' : current_house_years 
+            }
+            #st.write(result)
+
+        df_original = pd.read_csv(os.path.join('Training Data.csv'))
+        df_prediction = result
+        df_prediction = pd.DataFrame(df_prediction, index=[0])
+        st.write('Your Selected Options')
+        st.table(df_prediction)
+
+        # Initial transformation for prediction dataframe
+        df_original.drop(columns='Id', inplace=True)
+
+        # Cleaning Data
+        df_prediction = 
+
+        # Encoding Data
+
+        # Generation
+        # Function to determine the generation based on age
+        def assign_generation(age):
+            if age <= 27:
+                return 'Generation Z'
+            elif age <= 43:
+                return 'Generation Millenials'
+            elif age < 59:
+                return 'Generation X'
+            elif age < 69:
+                return 'Boomers II'
+            elif age <= 78:
+                return 'Boomers I'
+            else:
+                return 'Other'
+        
+        df_prediction['Generation'] = df_prediction['Age'].apply(assign_generation)
+
+        df_prediction['Generation'].replace({'Generation Z': 0,
+                                    'Generation Millenials': 1,
+                                    'Generation X' : 2,
+                                    'Boomers II' : 3,
+                                    'Boomers I' : 4,
+                                    'Other' : 5},inplace=True)
+
+        df_prediction['Generation'] = df_prediction['Generation'].astype('int64')
+
+    
+
+
+
+        # Scaling Data
+        ms = MinMaxScaler()
+        df_original_scaled = ms.fit_transform(df_original)
+        df_prediction_scaled = ms.transform(df_prediction)
+
+        # Prediction Section
+        st.subheader('Prediction Result')
+        single_array = np.array(df_prediction_scaled).reshape(1, -1)
+
+        model = load_model('tuned_random_forest_model.joblib')
+
+        prediction = model.predict(single_array)
+
+        if prediction == 0 :
+            st.info('This costumer has low risk of default, Loan approved')
+        elif prediction == 1 :
+            st.info('This costumer has high risk of default, Loan not approved')
+
 
 # Load the trained model
 model = joblib.load('tuned_random_forest_model.joblib')
@@ -11,17 +138,6 @@ scaler = joblib.load('minmax_scaler.joblib')
 # Create a Streamlit interface to take user input and make predictions
 st.title('Loan Prediction Model Test and Deployment')
 
-# Add input elements for the encoded features using selectbox and number_input
-# feature_id = st.number_input('Number of Passengers')
-feature_income = st.number_input('Purchase Lead Amount')
-feature_age = st.number_input('Length of Stay (Hour)')
-feature_experience = st.number_input('Flight Hour')
-feature_marital_status = st.number_input('Route')
-feature_house_ownership = st.number_input('Booking Origin')
-feature_car_ownership = st.number_input('Wants Extra Baggage')
-feature_profession = st.number_input('Wants Preferred Seats')
-feature_city = st.number_input('Wants In-Flight Meals')
-feature_state = st.number_input('Flight Duration')
 
 # Generation
 # Function to determine the generation based on age
@@ -120,7 +236,6 @@ if st.button('Predict'):
 
     # Reshape the array to make it 2D
     reshaped_inputs = user_inputs_array.reshape(1, -1)
-
 
     # Make predictions
     prediction = model.predict(reshaped_inputs)
